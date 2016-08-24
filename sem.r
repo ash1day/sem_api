222620a89dc77741635bf126ac112f79ac11fafa
y4ashida <y4ashida@gmail.com>
2016-08-24 19:46:08 +0900

    データセットPoliticalDemocracyでのテストを追加

--- sem.r
+++ sem.r
@@ -7,5 +7,5 @@ elems <- readLines("./tmp/elems.lav")
 
 cov <- getCov(elems[1], names=strsplit(elems[2], " ", fixed = TRUE)[[1]])
 fit <- sem(model, sample.cov=cov, sample.nobs=nobs)
-summary(fit, standardized=TRUE)
+summary(fit, fit.measures=TRUE)
 fitMeasures(fit, fit.measures = "all", baseline.model = NULL)
